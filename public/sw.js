f82eb068b88207b69c903bf6c567f53d57d9c5a9
bafplus <bafplus@users.noreply.github.com>
2026-03-30 13:39:41 +0200

    Disable service worker caching to fix HTTPS auth issue

--- public/sw.js
+++ public/sw.js
@@ -1,34 +1,12 @@
-const CACHE_NAME = 'tornops-v2';
-const urlsToCache = [
-    '/',
-    '/manifest.json',
-    '/images/tornops-shield-transparant.png',
-    '/images/tornops-shield-text-transparant.png',
-    '/images/tornops-shield-background.png',
-];
+const CACHE_NAME = 'tornops-v3';
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(urlsToCache))
-    );
     self.skipWaiting();
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request).then((networkResponse) => {
-                    return networkResponse;
-                }).catch(() => {
-                    return caches.match('/');
-                });
-            })
-    );
+    // Don't cache - always fetch from network
+    event.respondWith(fetch(event.request));
 });
 
 self.addEventListener('activate', (event) => {
@@ -36,26 +14,10 @@ self.addEventListener('activate', (event) => {
         caches.keys().then((cacheNames) => {
             return Promise.all(
                 cacheNames.map((cacheName) => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
+                    return caches.delete(cacheName);
                 })
             );
         })
     );
     self.clients.claim();
-});
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
 });
